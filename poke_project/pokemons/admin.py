06cd29789daf682b8f89b22142b26a2451db18f9
Diego Gómez <dagomez2530@gmail.com>
2021-05-11 19:36:25 -0500

    Creación de base de datos con sus respectivas tablas(Pokemon, PokemonEvolucion, Evolucion, Usuarios) y login, registro, logout para el usuario

---
+++ poke_project/pokemons/admin.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from .models import *
+# Register your models here.
+
+admin.site.register(Perfil)
+admin.site.register(Pokemon)
+admin.site.register(PokemonEvolucion)
+admin.site.register(Evolucion)
